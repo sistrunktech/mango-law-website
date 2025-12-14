5cbdf01f592af221786eeca936721d06ac760cd6
Tim Sistrunk <tim@sistrunktech.com>
2025-12-13 19:08:33 -0500

    Generate sitemap.xml during Vite build

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,10 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import ogPlugin from './plugins/vite-og-plugin';
+import sitemapPlugin from './plugins/vite-sitemap-plugin';
 
 export default defineConfig({
-  plugins: [react(), ogPlugin()],
+  plugins: [react(), ogPlugin(), sitemapPlugin()],
   server: {
     port: 5173,
   },
