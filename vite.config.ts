f5e875896eb115f867ac6f6383acf9bb7c48ac89
Tim Sistrunk <147546993+sistech2021@users.noreply.github.com>
2025-12-14 19:57:30 -0500

    chore(perf): add bundle analyzer (#26)

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,25 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { visualizer } from 'rollup-plugin-visualizer';
 import ogPlugin from './plugins/vite-og-plugin';
 import sitemapPlugin from './plugins/vite-sitemap-plugin';
 
-export default defineConfig({
-  plugins: [react(), ogPlugin(), sitemapPlugin()],
+export default defineConfig(({ mode }) => ({
+  plugins: [
+    react(),
+    ogPlugin(),
+    sitemapPlugin(),
+    mode === 'analyze'
+      ? visualizer({
+          filename: 'reports/bundle-visualizer.html',
+          gzipSize: true,
+          brotliSize: true,
+          template: 'treemap',
+          open: false,
+        })
+      : null,
+  ].filter(Boolean),
   server: {
     port: 5173,
   },
-});
+}));
